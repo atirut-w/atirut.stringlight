9f1748616a092cd01a366d77b4c28ee159161f07
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-06-12 21:23:16 +0700

    10 FPS timer

--- stringlight.gd
+++ stringlight.gd
@@ -1,2 +1,13 @@
 class_name StringLight
 extends Light3D
+
+
+var total_time := 0.0
+var threshold := 1.0 / 10.0
+
+
+func _physics_process(delta: float) -> void:
+	total_time += delta
+	if total_time >threshold:
+		total_time = 0
+		print(total_time)
