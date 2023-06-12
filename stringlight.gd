4eaa26545073f6801c216e88a1ddef8ed9fb9f58
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2023-06-12 21:35:29 +0700

    Implement light styles

--- stringlight.gd
+++ stringlight.gd
@@ -2,8 +2,40 @@ class_name StringLight
 extends Light3D
 
 
+@export var light_style: LightStyle
+
 var total_time := 0.0
 var threshold := 1.0 / 10.0
+
+enum LightStyle {
+	NORMAL,
+	FLICKER_A,
+	SLOW_STRONG_PULSE,
+	CANDLE_A,
+	FAST_STROBE,
+	GENTLE_PULSE,
+	FLICKER_B,
+	CANDLE_B,
+	CANDLE_C,
+	SLOW_STROBE,
+	FLUORESCENT_FLICKER,
+	SLOW_PULSE_NOBLACK
+}
+
+const light_anim: Array[String] = [
+	"m",
+	"mmnmmommommnonmmonqnmmo",
+	"abcdefghijklmnopqrstuvwxyzyxwvutsrqponmlkjihgfedcba",
+	"mmmmmaaaaammmmmaaaaaabcdefgabcdefg",
+	"mamamamamama",
+	"jklmnopqrstuvwxyzyxwvutsrqponmlkj",
+	"nmonqnmomnmomomno",
+	"mmmaaaabcdefgmmmmaaaammmaamm",
+	"mmmaaammmaaammmabcdefaaaammmmabcdefmmmaaaa",
+	"aaaaaaaazzzzzzzz",
+	"mmamammmmammamamaaamammma",
+	"abcdefghijklmnopqrrqponmlkjihgfedcba"
+]
 
 
 func _physics_process(delta: float) -> void:
